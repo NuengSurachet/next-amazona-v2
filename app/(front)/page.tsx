a07a03346d4f51871289bd69f99dc328b1dd28b5
F_9.1_Icon_Nueng <149551654+NuengSurachet@users.noreply.github.com>
2024-03-11 17:52:16 +0700

    learning
    นาที ที่ 55:53

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,7 +1,14 @@
+import data from "@/lib/data";
+import ProductItem from "@/components/product/productItem";
 export default function Home() {
   return (
     <>
       <h2 className="text-2xl py-2">latest products</h2>
+      <div className="grid grid-cols-1  gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((products)=>(
+          <ProductItem key={products.slug} product={products} />
+        ))}
+      </div>
     </>
   );
 }
